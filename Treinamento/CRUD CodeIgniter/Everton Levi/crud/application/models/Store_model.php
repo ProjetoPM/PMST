9189811276cc320de595e13898bb5534d3b2fc94
Everton Levi <everton.levi@hotmail.com>
2018-07-17 16:03:02 -0300

    Teste Everton Levi

---
+++ Treinamento/CRUD CodeIgniter/Everton Levi/crud/application/models/Store_model.php
@@ -0,0 +1,13 @@
+<?php
+class Store_model extends CI_Model {
+	function __construct(){
+		parent::__construct();
+		$this->load->database();
+	}
+	public function getAllStores(){
+		$query = $this->db->get('store'); 
+		return $query->result();
+
+	}
+}
+?>
