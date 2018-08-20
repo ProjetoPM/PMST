ad287cb4f2f102a8448d2fbc4a76b5fb5a88ce6f
Lucas Abner Leal Silva <lucasabner98@outlook.com>
2018-08-20 12:49:38 -0300

    Crud PMQ Pronto

--- application/controllers/Charter_Quality.php
+++ application/controllers/Charter_Quality.php
@@ -1,57 +1,54 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class GerenciarCustos extends CI_Controller {
+class Charter_Quality extends CI_Controller {
 
 	function __construct(){
 		parent::__construct();
 		$this->load->helper('url');
 		$this->load->model('Quality_model');
 	}
-
+ 
 	public function addnew($project_id){
-		$dado['quality_mp'] = $this->Custos_model->getAllQuality();
+		$dado['quality_mp'] = $this->Quality_model->readQuality($project_id);
 		$dado['id'] = $project_id;
+		$this->db->where('project_id', $project_id);
+		$dado['project'] =  $this->db->get('project')->result();
+		
 		//$dado['verific'] = true;
+		$this->db->where('project_id', $project_id);
+        $dataproject['project'] = $this->db->get('project')->result();
 		$this->load->view('frame/header_view');
-        $this->load->view('frame/sidebar_nav_view');
-	 	$this->load->view('project/quality',$dado);
+		$this->load->view('frame/sidebar_nav_view');
+	
+			$this->load->view('project/quality_mp',$dado);
+	
 	 }
 
-	public function insert($id){
-		$dado['quality_mp'] = $this->Custos_model->getAllQuality();
-		if($dado['quality_mp']!=null){
-			$query = $this->Quality_model->deletequality($id);
-		}
+	 function create()
+	 {
+		 //$this->ajax_checking();
+		 
+		 $postData = $this->input->post();
+		 $insert   = $this->Quality_model->createQuality($postData);
+		 redirect('project/' . $postData['project_id']);
+		 echo json_encode($insert);
+	 }
 
+
+	 public function update($id){
 		$quality_mp['methodology'] = $this->input->post('methodology');
 		$quality_mp['related_processes'] = $this->input->post('related_processes');
 		$quality_mp['expectations_tolerances'] = $this->input->post('expectations_tolerances');
 		$quality_mp['traceability'] = $this->input->post('traceability');
-		$quality_mp['project_id'] = $id;
-		$quality_mp['status'] = 1;
-		$query = $this->Quality_model->insertquality($quality_mp);
+		$quality_mp['status'] = (int) $this->input->post('status');
 
-		if($query){
+		//$insert = $this->project_model->insert_project_pgq($quality_mp);
+		$query = $this->Quality_model->updateQuality($quality_mp, $id);
+
 		$this->load->view('frame/header_view');
         $this->load->view('frame/sidebar_nav_view');
-        redirect('project/' . $quality_mp['project_id']);
-		}
-
+        redirect('project/' . $id);
 	}
-		$this->load->view('project/project_page',$id);
-			//header('location:'.base_url().$this->index($id));
-		}
-
-	}
-
-	public function delete($project_id){
-		$query = $this->Project_model->deleteproject($project_id);
-
-		if($query){
-			header('location:'.base_url().$this->index());
-		}
-	}
-
 }
 ?>
--- application/views/project/top-menu.php
+++ application/views/project/top-menu.php
@@ -7,25 +7,14 @@
          <div class="panel-body text-justify">
             <div class="panel-body text-center">
                <h2 class="section-title mb-2 h1"><?= $project[0]->title;?></h2>
-               <a class="btn btn-workflow btn-lg" href="<?=base_url("Project/cost".$project[0]->project_id)?>">
+               <a class="btn btn-workflow btn-lg" href="<?=base_url("Project/cost/".$project[0]->project_id)?>">
                DASHBOARD
                </a>
-               <a class="btn btn-workflow btn-lg" href="<?=base_url("Charter_Quality/addnew".$project[0]->project_id) ?>">
-               PMQ
+               <a class="btn btn-workflow btn-lg" href="<?=base_url()?>Charter_Quality/addnew/<?php echo $project[0]->project_id;?>">PMQ</a>
                <a class="btn btn-workflow btn-lg" href="<?=base_url("GerenciarCustos/addnew/".$project[0]->project_id)?>">
                COST
                </a>
-<<<<<<< HEAD
-               <a class="btn btn-workflow btn-lg" href="<?=base_url("Charter_Quality/addnew".$project[0]->project_id) ?>">
-               PMQ
-=======
-               <a class="btn btn-workflow btn-lg" href="<?=base_url("metodo3/".$project[0]->project_id) ?>">
-               BOTAO 3
-               </a>
-               <a class="btn btn-workflow btn-lg" href="">
-               BOTAO ETC
->>>>>>> master
-               </a>
+              
             </div>
          </div>
       </div>
