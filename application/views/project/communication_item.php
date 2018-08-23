f5435b6c4563b214d7e8a11f8ff62d9cec72af0c
icarocrespo <icarocrespo@gmail.com>
2018-08-23 14:19:52 -0300

    Communication_item view

--- application/views/project/communication_item.php
+++ application/views/project/communication_item.php
@@ -218,13 +218,11 @@
 </div>
 <div class="col-lg-6">
     <div class="dropdown">
-    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">	
     Dropdown button
     </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
     <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
   </div>
 </div>
 </div>
