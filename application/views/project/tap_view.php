b8c15bca0ffddc151b9d57c56b9b1cb66b13ee80
Rafael S. Maria <rafaelsmaria01@gmail.com>
2018-08-28 15:00:22 -0300

    Bug fixed.

--- application/views/project/tap_view.php
+++ application/views/project/tap_view.php
@@ -15,6 +15,7 @@
 
 					<div class="form-group">
 						<label for="project_description">Project Description</label>
+						<a href="#" data-toggle="tooltip" title="Description of your project!">?</a>
 						<div >                     
 							<textarea class="form-control" id="project_description" name="project_description"></textarea>
 						</div>
@@ -22,6 +23,7 @@
 
 					<div class="form-group">
 						<label for="project_purpose">Project Purpose</label>
+						<a href="#" data-toggle="tooltip" title="Purpose of your project!">?</a>
 						<div >                     
 							<textarea class="form-control" id="project_purpose" name="project_purpose"></textarea>
 						</div>
@@ -29,6 +31,7 @@
 
 					<div class="form-group">
 						<label for="project_objective">Project Objectives</label>
+						<a href="#" data-toggle="tooltip" title="Objectives of your project!">?</a>
 						<div >                     
 							<textarea class="form-control" id="project_objective" name="project_objective"></textarea>
 						</div>
@@ -36,6 +39,7 @@
 
 					<div class="form-group">
 						<label for="benefits">Benefits</label>
+						<a href="#" data-toggle="tooltip" title="Benefits of your project!">?</a>
 						<div >                     
 							<textarea class="form-control" id="benefits" name="benefits"></textarea>
 						</div>
@@ -43,6 +47,7 @@
 
 					<div class="form-group">
 						<label for="high_level_requirements">High Level Requirements</label>
+						<a href="#" data-toggle="tooltip" title="Description of requirements in high level!">?</a>
 						<div >                     
 							<textarea class="form-control" id="high_level_requirements" name="high_level_requirements"></textarea>
 						</div>
@@ -50,6 +55,7 @@
 
 					<div class="form-group">
 						<label for="initial_assumptions">Initial Assumptions</label>
+						<a href="#" data-toggle="tooltip" title="Assumptions of start your projetc!">?</a>
 						<div >                     
 							<textarea class="form-control" id="initial_assumptions" name="initial_assumptions"></textarea>
 						</div>
@@ -57,6 +63,7 @@
 
 					<div class="form-group">
 						<label for="initial_restrictions">Initial Restrictions</label>
+						<a href="#" data-toggle="tooltip" title="Restrictions initial of your project!">?</a>
 						<div >                     
 							<textarea class="form-control" id="initial_restrictions" name="initial_restrictions"></textarea>
 						</div>
@@ -64,6 +71,7 @@
 
 					<div class="form-group">
 						<label for="project_limits">Project Limits</label>
+						<a href="#" data-toggle="tooltip" title="Limits of your project">?</a>
 						<div >                     
 							<textarea class="form-control" id="project_limits" name="project_limits"></textarea>
 						</div>
@@ -71,6 +79,7 @@
 
 					<div class="form-group">
 						<label for="high_level_risks">High Level Risks</label>
+						<a href="#" data-toggle="tooltip" title="Risks of your project in high level!">?</a>
 						<div >                     
 							<textarea class="form-control" id="high_level_risks" name="high_level_risks"></textarea>
 						</div>
@@ -78,6 +87,7 @@
 
 					<div class="form-group">
 						<label for="summary_schedule">Summary Schedule</label>
+						<a href="#" data-toggle="tooltip" title="detail of schedule of your project!">?</a>
 						<div >                     
 							<textarea class="form-control" id="summary_schedule" name="summary_schedule"></textarea>
 						</div>
@@ -85,6 +95,7 @@
 
 					<div class="form-group">
 						<label for="budge_summary">Budge Summary</label>
+						<a href="#" data-toggle="tooltip" title="Budget summary of your project!">?</a>
 						<div >                     
 							<textarea class="form-control" id="budge_summary" name="budge_summary"></textarea>
 						</div>
@@ -92,6 +103,7 @@
 
 					<div class="form-group">
 						<label for="project_approval_requuirements">Project Approval Requirements</label>
+						<a href="#" data-toggle="tooltip" title="Requirements for approval your project!">?</a>
 						<div>
 							<textarea class="form-control" id="project_approval_requuirements" name="project_approval_requuirements"></textarea>
 						</div>
@@ -124,9 +136,9 @@
 
 						<div class="form-group">
 
-							<label>Enter Start Date:</label><br>
+							<label>Enter Start Date:</label><a href="#" data-toggle="tooltip" title="Date start of your project!">?</a><br>
 							<input type="date" name="start_date" max="2017-12-31"><br>
-							<label>Enter End Date:</label><br>
+							<label>Enter End Date:</label><a href="#" data-toggle="tooltip" title="Date finish of your project!">?</a><br>
 							<input type="date" name="end_date" min="2025-01-02"><br></br>
 
 							<div class="form-group">
@@ -145,77 +157,77 @@
 
 							<input type="hidden" name="project_id" value="<?php echo $project_id;?>">                           
 							<div class="form-group">
-								<label for="project_description">Project Description  <a href="#" data-toggle="tooltip" title="Description of your project!">?</a></label>
+								<label for="project_description">Project Description  </label>
 								<div >                     
 									<textarea class="form-control" id="project_description" name="project_description"><?php echo $project_charter[0]->project_description; ?></textarea>
 								</div>
 							</div>
 
 							<div class="form-group">
-								<label for="project_purpose">Project Purpose  <a href="#" data-toggle="tooltip" title="não entendi. igual o project purpose!">?</a></label>
+								<label for="project_purpose">Project Purpose  </label>
 								<div >                     
 									<textarea class="form-control" id="project_purpose" name="project_purpose"><?php echo $project_charter[0]->project_purpose; ?></textarea>
 								</div>
 							</div>
 
 							<div class="form-group">
-								<label for="project_objective">Project Objectives  <a href="#" data-toggle="tooltip" title="Objectives of your project!">?</a></label>
+								<label for="project_objective">Project Objectives  </label>
 								<div >                     
 									<textarea class="form-control" id="project_objective" name="project_objective"><?php echo $project_charter[0]->project_objective; ?></textarea>
 								</div>
 							</div>
 
 							<div class="form-group">
-								<label for="benefits">Benefits  <a href="#" data-toggle="tooltip" title="Benefits of your project!">?</a></label>
+								<label for="benefits">Benefits  </label>
 								<div >                     
 									<textarea class="form-control" id="benefits" name="benefits"><?php echo $project_charter[0]->benefits; ?></textarea>
 								</div>
 							</div>
 
 							<div class="form-group">
-								<label for="high_level_requirements">High Level Requirements  <a href="#" data-toggle="tooltip" title="Description of requirements in high level!">?</a></label>
+								<label for="high_level_requirements">High Level Requirements  </label>
 								<div >                     
 									<textarea class="form-control" id="high_level_requirements" name="high_level_requirements"><?php echo $project_charter[0]->high_level_requirements; ?></textarea>
 								</div>
 							</div>
 
 							<div class="form-group">
-								<label for="initial_assumptions">Initial Assumptions  <a href="#" data-toggle="tooltip" title="Assumptions of start your projetc!">?</a></label>
+								<label for="initial_assumptions">Initial Assumptions  </label>
 								<div >                     
 									<textarea class="form-control" id="initial_assumptions" name="initial_assumptions"><?php echo $project_charter[0]->initial_assumptions; ?></textarea>
 								</div>
 							</div>
 
 							<div class="form-group">
-								<label for="initial_restrictions">Initial Restrictions  <a href="#" data-toggle="tooltip" title="Restrictions initial of your project!">?</a></label>
+								<label for="initial_restrictions">Initial Restrictions  </label>
 								<div >                     
 									<textarea class="form-control" id="initial_restrictions" name="initial_restrictions"><?php echo $project_charter[0]->initial_restrictions; ?></textarea>
 								</div>
 							</div>
 
 							<div class="form-group">
-								<label for="project_limits">Project Limits  <a href="#" data-toggle="tooltip" title="Limited your project">?</a></label>
+								<label for="project_limits">Project Limits  </label>
 								<div >                     
 									<textarea class="form-control" id="project_limits" name="project_limits"><?php echo $project_charter[0]->project_limits; ?></textarea>
 								</div>
 							</div>
 
 							<div class="form-group">
-								<label for="high_level_risks">High Level Risks  <a href="#" data-toggle="tooltip" title="Risks of your project in nivel high!">?</a></label>
+								<label for="high_level_risks">High Level Risks  </label>
 								<div >                     
 									<textarea class="form-control" id="high_level_risks" name="high_level_risks"><?php echo $project_charter[0]->high_level_risks; ?></textarea>
 								</div>
 							</div>
 
 							<div class="form-group">
-								<label for="summary_schedule">Summary Schedule  <a href="#" data-toggle="tooltip" title="detail of schedule of your project!">?</a></label>
+								<label for="summary_schedule">Summary Schedule  </label>
 								<div >                     
 									<textarea class="form-control" id="summary_schedule" name="summary_schedule"><?php echo $project_charter[0]->summary_schedule; ?></textarea>
 								</div>
 							</div>
 
 							<div class="form-group">
-								<label for="budge_summary">Budge Summary  <a href="#" data-toggle="tooltip" title="Budget summary of your project!">?</a></label>
+								<label for="budge_summary">Budge Summary  </label>
 								<div >                     
 									<textarea class="form-control" id="budge_summary" name="budge_summary"><?php echo $project_charter[0]->budge_summary; ?></textarea>
 								</div>
@@ -247,17 +259,17 @@
 								</div>
 
 								<div class="form-group">
-									<label for="project_approval_requirements">Project Approval Requirements  <a href="#" data-toggle="tooltip" title="Requirements for approval your project!">?</a></label>
+									<label for="project_approval_requirements">Project Approval Requirements  </label>
 									<div>
 										<textarea class="form-control" id="project_approval_requirements" name="project_approval_requirements"><?php echo $project_charter[0]->project_approval_requirements; ?></textarea>
 									</div>
 								</div>
 
 								<div class="form-group">
-									<label>Enter Start Date:  <a href="#" data-toggle="tooltip" title="Date start of your project!">?</a></label><br>
-									<input type="date" name="start_date" max="2017-12-31"><br><br>
-									<label>Enter End Date:  <a href="#" data-toggle="tooltip" title="Date finish of your project!">?</a></label><br>
-									<input type="date" name="end_date" min="2025-01-02"><br></br>
+									<label>Enter Start Date:  </label><br>
+									<input type="date" name="start_date" max="2017-12-31" value="<?php echo $project_charter[0]->start_date; ?>"><br><br>
+									<label>Enter End Date:  </label><br>
+									<input type="date" name="end_date" min="2025-01-02" value="<?php echo $project_charter[0]->end_date; ?>"><br></br>
 
 									<?php if ($project_charter[0]->status == 1){ ?>
 										<input type="radio" checked name="status" value="1">
@@ -268,9 +280,9 @@
 									<?php } else { ?>
 
 										<input type="radio" name="status" value="1">
-										<label>On  <a href="#" data-toggle="tooltip" title="Hooray!">?</a></label><br>
+										<label>On  </label><br>
 										<input type="radio" checked name="status" value="0">
-										<label>Off  <a href="#" data-toggle="tooltip" title="Hooray!">?</a></label>
+										<label>Off </label>
 
 									<?php } ?>
 									<input id="tap-submit" type="submit" value="Save" class="btn btn-lg btn-success btn-block">
