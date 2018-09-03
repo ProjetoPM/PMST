f9f90cb76a88b41dc9c3637933e7cff65cb6dabe
Everton__Levi <everton.levi@hotmail.com>
2018-09-03 16:48:08 -0300

    Schedule
    Inclusão do nov layout / tooltips inseridos / Falta editar detalhes de config dos novos botões

--- application/controllers/Schedule.php
+++ application/controllers/Schedule.php
@@ -11,7 +11,7 @@ class Schedule extends CI_Controller
 
 		$this->lang->load('btn','english');
         // $this->lang->load('btn','portuguese-brazilian');
-        $this->lang->load('schedule','english');
+		$this->lang->load('schedule','english');
         // $this->lang->load('schedule','portuguese-brazilian');
 
 
@@ -34,9 +34,9 @@ class Schedule extends CI_Controller
 //Criar o Schedule 1 Vez
 	public function createSchedule(){
 		$postData = $this->input->post();
-  $insert = $this->Schedule_model->createSchedule($postData);
- redirect('project/' . $postData['project_id']);
- echo json_encode($insert);
+		$insert = $this->Schedule_model->createSchedule($postData);
+		redirect('project/' . $postData['project_id']);
+		echo json_encode($insert);
 
 	//	$dados = $this->post('schedule_mp.schedule_model');
 	}
@@ -58,7 +58,7 @@ class Schedule extends CI_Controller
 		redirect('project/' . $id);
 		//if ($query) {
 		//header('location:'.base_url().//$this->schedule_form());
-		}
+	}
 
 
 
--- application/views/schedule.php
+++ application/views/schedule.php
@@ -26,44 +26,60 @@
 
 				<input type="hidden" name="project_id" value="<?php echo $project[0]->project_id; ?>">
 
-
-				<div class="form-group">
-					<label>Shedule Model</label>
-					<textarea class="form-control" name="schedule_model"></textarea>
+				<div class="col-lg-12">
+					<div class="form-group">
+						<label>Shedule Model</label><a class="btn-sm btn-default" data-toggle="tooltip" data-placement="right" title="<?=$this->lang->line('human_resource-roles-tooltip')?>"><i class="glyphicon glyphicon-comment"></i></a>
+						<textarea class="form-control" name="schedule_model"></textarea>
+					</div>
 				</div>
 
-				<div class="form-group">
-					<label>Accuary Leve</label>  
-					<textarea class="form-control"  name="accuracy_level"></textarea>
+				<div class="col-lg-6">
+					<div class="form-group">
+						<label>Accuary Leve</label><a class="btn-sm btn-default" data-toggle="tooltip" data-placement="right" title="<?=$this->lang->line('human_resource-roles-tooltip')?>"><i class="glyphicon glyphicon-comment"></i></a> 
+						<textarea class="form-control"  name="accuracy_level"></textarea>
+					</div>
 				</div>
 
-
-				<div class="form-group">
-					<label>Organizational Procedures</label>  
-					<textarea class="form-control"  name="organizational_procedures"></textarea>
+				<div class="col-lg-6">
+					<div class="form-group">
+						<label>Organizational Procedures</label><a class="btn-sm btn-default" data-toggle="tooltip" data-placement="right" title="<?=$this->lang->line('human_resource-roles-tooltip')?>"><i class="glyphicon glyphicon-comment"></i></a>  
+						<textarea class="form-control"  name="organizational_procedures"></textarea>
+					</div>
 				</div>
 
-
-				<div class="form-group">
-					<label>Shedule Maintenance</label>  
-					<textarea class="form-control"  name="schedule_maintenance"></textarea>
+				<div class="col-lg-12">
+					<div class="form-group">
+						<label>Shedule Maintenance</label><a class="btn-sm btn-default" data-toggle="tooltip" data-placement="right" title="<?=$this->lang->line('human_resource-roles-tooltip')?>"><i class="glyphicon glyphicon-comment"></i></a>  
+						<textarea class="form-control"  name="schedule_maintenance"></textarea>
+					</div>
 				</div>
 
-				<div class="form-group">
-					<label>Performance Measurement</label>					
-					<textarea class="form-control" name="performance_measurement"></textarea>
+				<div class="col-lg-6">
+					<div class="form-group">
+						<label>Performance Measurement</label><a class="btn-sm btn-default" data-toggle="tooltip" data-placement="right" title="<?=$this->lang->line('human_resource-roles-tooltip')?>"><i class="glyphicon glyphicon-comment"></i></a>					
+						<textarea class="form-control" name="performance_measurement"></textarea>
+					</div>
 				</div>
 
 				<!-- Textarea-->
-				<div class="form-group">
-					<label>Report Format</label>
-					<textarea class="form-control" name="report_format"></textarea>
-
+				<div class="col-lg-6">
+					<div class="form-group">
+						<label>Report Format</label><a class="btn-sm btn-default" data-toggle="tooltip" data-placement="right" title="<?=$this->lang->line('human_resource-roles-tooltip')?>"><i class="glyphicon glyphicon-comment"></i></a>
+						<textarea class="form-control" name="report_format"></textarea>
+					</div>
 				</div>
 
 
-				<button type="submit" class="btn btn-lg btn-success btn-block">Save</button> 
 			</form>
+			<div class="col-lg-12">
+				<button id="new_human_resource-submit" type="submit" value="Save" class="btn btn-lg btn-success pull-right">
+					<i class="glyphicon glyphicon-ok"></i> <?=$this->lang->line('btn-save')?>
+				</button> 
+
+				<form action="<?php echo base_url('project/'); ?><?php echo $id; ?>" >
+					<button class="btn btn-lg btn-info pull-left" >  <i class="glyphicon glyphicon-chevron-left"></i> <?=$this->lang->line('btn-back')?></button>
+				</form>
+			</div>
 
 		<?php }else{
 			foreach ($schedule_mp as $schedule) {
@@ -84,8 +100,8 @@
 					<div class="form-group">
 						<label>Shedule Model</label>
 						<textarea class="form-control" id="schedule_model" name="schedule_model"><?php  
-							echo $schedule->schedule_model;
-							?></textarea>
+						echo $schedule->schedule_model;
+						?></textarea>
 					</div>
 
 
@@ -93,8 +109,8 @@
 					<div class="form-group">
 						<label>Accuary Leve</label>  
 						<textarea class="form-control" id="accuracy_level" name="accuracy_level"><?php  
-							echo $schedule->accuracy_level; 
-							?></textarea>
+						echo $schedule->accuracy_level; 
+						?></textarea>
 					</div>
 
 
@@ -102,8 +118,8 @@
 					<div class="form-group">
 						<label>Organizational Procedures</label>  
 						<textarea class="form-control" id="organizational_procedures" name="organizational_procedures"><?php  
-							echo $schedule->organizational_procedures; 
-							?></textarea>
+						echo $schedule->organizational_procedures; 
+						?></textarea>
 					</div>
 
 
@@ -111,24 +127,24 @@
 					<div class="form-group">
 						<label>Shedule Maintenance</label>  
 						<textarea class="form-control" id="schedule_maintenance" name="schedule_maintenance"><?php  
-							echo $schedule->schedule_maintenance; 
-							?></textarea>
+						echo $schedule->schedule_maintenance; 
+						?></textarea>
 					</div>
 
 					<!-- Textarea-->
 					<div class="form-group">
 						<label>Performance Measurement</label>					
 						<textarea class="form-control" id="performance_measurement" name="performance_measurement"><?php  
-							echo $schedule->performance_measurement; 
-							?></textarea>
+						echo $schedule->performance_measurement; 
+						?></textarea>
 					</div>
 
 					<!-- Textarea-->
 					<div class="form-group">
 						<label>Report Format</label>						
 						<textarea class="form-control" id="report_format" name="report_format"><?php  
-							echo $schedule->report_format; 
-							?></textarea>
+						echo $schedule->report_format; 
+						?></textarea>
 
 					</div>
 
