1f55a02e783bd52d09d189fc4ecdba19cff109a3
Wagner Inácio <wagner.inacio16@outlook.com>
2018-09-04 16:51:41 -0300

    Stakeholder_MP
    Fiz a remodelagem da view; inserção dos dados de linguagem; criação da form group - form-control; Criação do arquivo "satakeholder_mp_lang.php"// Falta inserir alguns tooltip; edição do  arquivo de linguagem com a view;

--- application/views/project/stakeholder_mp.php
+++ application/views/project/stakeholder_mp.php
@@ -1,7 +1,7 @@
 <div id="page-wrapper">
   <div class="row">
     <div class="col-lg-12">
-      <h1 class="page-header">Gerenciar Partes Interessadas</h1>
+      <h1 class="page-header"><?=$this->lang->line('stakeholder_mp-title')?></h1>
     </div>
     <!-- /.col-lg-12 -->
   </div>
@@ -28,9 +28,11 @@
 
 
   
-  <div>
+  <div class="col-lg-6 form-group">
 <label for="stakeholder"> Select Stakeholder</label>
- <select name="stakeholder_id">
+<a class="btn-sm btn-default" data-toggle="tooltip" data-placement="right" title="<?=$this->lang->line('select-stakholder-tooltip')?>"><i class="glyphicon glyphicon-comment"></i></a>
+
+ <select name="stakeholder_id" class="form-control">
 
         <?php
           foreach ($stakeholders as $stakeholder) {
@@ -47,9 +49,9 @@
 
             <!-- Text input-->
           <!-- Textarea -->
-               <div class="form-group">
+      <div class="col-lg-3 form-group">
           <label for="interest">Interest Level</label>
-          <select name="interest">
+          <select name="interest" class="form-control">
             <option value="10">10%</option>
             <option value="30">30%</option>
             <option value="50">50%</option>
@@ -57,9 +59,9 @@
             <option value="90">90%</option>
           </select>
       </div>
-        <div class="form-group">
+        <div class="col-lg-3 form-group">
           <label for="power">Power Level</label>
-          <select name="power">
+          <select name="power" class="form-control">
             <option value="10">10%</option>
             <option value="30">30%</option>
             <option value="50">50%</option>
@@ -67,9 +69,9 @@
             <option value="90">90%</option>
           </select>
       </div>
-        <div class="form-group">
+        <div class="col-lg-3 form-group">
           <label for="influence">Influence Level</label>
-          <select name="influence">
+          <select name="influence" class="form-control">
             <option value="10">10%</option>
             <option value="30">30%</option>
             <option value="50">50%</option>
@@ -77,9 +79,9 @@
             <option value="90">90%</option>
           </select>
       </div>
-        <div class="form-group">
+        <div class="col-lg-3 form-group">
           <label for="impact">Impact Level</label>
-          <select name="impact">
+          <select name="impact" class="form-control">
             <option value="10">10%</option>
             <option value="30">30%</option>
             <option value="50">50%</option>
@@ -87,9 +89,9 @@
             <option value="90">90%</option>
           </select>
       </div>
-        <div class="form-group">
+        <div class="col-lg-3 form-group">
           <label for="current_engagement ">Current Engagement</label>
-          <select name="current_engagement">
+          <select name="current_engagement" class="form-control">
              <option value="unaware">Unaware</option>
             <option value="supportive">Supportive</option>
             <option value="leading">Leading</option>
@@ -97,9 +99,9 @@
             <option value="resistant">Resistant</option>
           </select>
       </div>
-       <div class="form-group">
+       <div class="col-lg-3 form-group">
           <label for="expected_engagement ">Expected Engagement</label>
-          <select name="expected_engagement">
+          <select name="expected_engagement" class="form-control">
             <option value="unaware">Unaware</option>
             <option value="supportive">Supportive</option>
             <option value="leading">Leading</option>
@@ -107,30 +109,39 @@
             <option value="resistant">Resistant</option>
           </select>
       </div>
-          <div class="form-group">
+          <div class="col-lg-12 form-group">
             <label for="description">Estratégia para Engajamento / Gerenciamento</label>
             <div >                     
-              <textarea class="form-control" id="description" name="strategy"></textarea>
+              <textarea class=" form-control elasticteste" oninput="eylem(this, this.value)"  id="description" name="strategy"></textarea>
             </div>
           </div>
 
-          <div class="form-group">
+          <div class="col-lg-6 form-group">
             <label for="description">Escopo e Impacto das Mudanças para a PI</label>
             <div >                     
-              <textarea class="form-control" id="description" name="scope"></textarea>
+              <textarea class="form-control elasticteste" oninput="eylem(this, this.value)" id="description" name="scope"></textarea>
             </div>
           </div>
 
-          <div class="form-group">
+          <div class="col-lg-6 form-group">
             <label for="description">Observações / Interrelações com outras PI</label>
             <div >                     
-              <textarea class="form-control" id="description" name="observation"></textarea>
+              <textarea class="col-lg-6 form-control elasticteste" oninput="eylem(this, this.value)" id="description" name="observation"></textarea>
             </div>
           </div>
+         <div class="col-lg-12">
+            <button id="new_human_resource-submit" type="submit" value="Save" class="btn btn-lg btn-success pull-right">
+            <i class="glyphicon glyphicon-ok"></i> <?=$this->lang->line('btn-save')?>
+            </button> 
+            </form>
 
-          <input  type="submit" value="Salvar" class="btn btn-lg btn-success btn-block">
-        </form>
+            <form action="<?php echo base_url('project/'); ?><?php echo $id; ?>" >
+                <button class="btn btn-lg btn-info pull-left" >  <i class="glyphicon glyphicon-chevron-left"></i> <?=$this->lang->line('btn-back')?></button>
+            </form>
+                
+         </div>
       </div>
+
       <!-- /.col-lg-12 -->
     </div>
     <!-- /.row -->
