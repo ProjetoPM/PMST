d18ba45ead76b488c2da0b90d0520eb519c3943a
Arthur Malmann Becker <arthur@porthal.com.br>
2018-09-13 14:09:45 -0300

    Preparaçao de mais alguns arquivos lang

---
+++ application/language/portuguese-brazilian/my-profile_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+// Title
+$lang['title'] = 'Suas Informações Pessoais';
+
+// Form
+$lang['name'] = 'Seu Nome';
+$lang['email'] = 'Seu Email';
+$lang['institution'] = 'Sua Instituição';
+
+$lang['member-since'] = '';
+
+$lang['change-password'] = 'Alterar Senha';
+
+?>
