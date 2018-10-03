6214e51ecf03845f8ee02906f73cf6f8b93cc6b3
Rafael S. Maria <rafaelsmaria01@gmail.com>
2018-10-02 21:26:10 -0300

    Implementation of frontend
    calendar adapted in view of project charter.

--- application/views/project/tap_view.php
+++ application/views/project/tap_view.php
@@ -396,40 +396,40 @@
 
 													<script type="text/javascript">
 
-      //////////////////////////////////
-      // Start Date & End Date
-      //////////////////////////////////
-      var currentDate = new Date();
-      var today = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 0, 0, 0, 0);
-      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+									      //////////////////////////////////
+									      // Start Date & End Date
+									      //////////////////////////////////
+									      var currentDate = new Date();
+									      var today = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 0, 0, 0, 0);
+									      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
 
-      var startDate = $("#start_date").datepicker({
-      	autoclose: true,
-      	format: 'yyyy/mm/dd',
-        //language: 'pt-BR', //Idioma do Calendario
-        container: container,
-        keyboardNavigation: true,
-        orientation: "bottom",
-        todayHighlight : true,
-        startDate: today,
-       }).on('changeDate', function(ev) {
-       	var newDate = new Date(ev.date.setDate(ev.date.getDate() + 1));
-       	endDate.datepicker("setStartDate", newDate);
-       });
+									      var startDate = $("#start_date").datepicker({
+									      	autoclose: true,
+									      	format: 'yyyy/mm/dd',
+									        //language: 'pt-BR', //Idioma do Calendario
+									        container: container,
+									        keyboardNavigation: true,
+									        orientation: "bottom",
+									        todayHighlight : true,
+									        startDate: today,
+									       }).on('changeDate', function(ev) {
+									       	var newDate = new Date(ev.date.setDate(ev.date.getDate() + 1));
+									       	endDate.datepicker("setStartDate", newDate);
+									       });
 
-      //Start Date Ends Here
-      var endDate = $("#end_date").datepicker({
-      	autoclose: true,
-      	format: 'yyyy/mm/dd',
-        //language: 'pt-BR', //Idioma do Calendario
-        container: container,
-        keyboardNavigation: true,
-        orientation: "bottom",
-        startDate: today,
-        /*todayHighlight : true,*/
-       });
-      //End Date Ends Here
+									      //Start Date Ends Here
+									      var endDate = $("#end_date").datepicker({
+									      	autoclose: true,
+									      	format: 'yyyy/mm/dd',
+									        //language: 'pt-BR', //Idioma do Calendario
+									        container: container,
+									        keyboardNavigation: true,
+									        orientation: "bottom",
+									        startDate: today,
+									        /*todayHighlight : true,*/
+									       });
+									      //End Date Ends Here
 
-     </script>
+									     </script>
 
      <?php $this->load->view('frame/footer_view')?>                    	
