9db663da21dd12e24e0da342a75df8d3617907fb
Arthur Malmann Becker <arthur@porthal.com.br>
2018-10-25 16:03:03 -0300

    lang project page parte1

---
+++ application/language/english/project-page_lang.php
@@ -0,0 +1,76 @@
+<?php
+$lang['phases'] = 'Phases';
+
+// Top Phases
+$lang['initiating'] = 'Initiating';
+$lang['planning'] = 'Planning';
+$lang['executing'] = 'Executing';
+$lang['monitoring-controlling'] = 'Monitoring & Controlling';
+$lang['closing'] = 'Closing';
+
+// Side Phases
+$lang['integration'] = 'Integration';
+$lang['scope'] = 'Scope';
+$lang['time'] = 'Time';
+$lang['cost'] = 'Cost';
+$lang['quality'] = 'Quaçity';
+$lang['hr'] = 'Human Resources';
+$lang['communication'] = 'Communication';
+$lang['risk'] = 'Risk';
+$lang['procurement'] = 'Procurement';
+$lang['stakeholder'] = 'Stakeholder';
+
+// 
+$lang['project-charter'] = 'Project Charter';
+$lang['stakeholder-register'] = 'Stakeholder Register';
+$lang['project-management'] = 'Project Management';
+$lang['requirement-management'] = 'Requirement Management';
+$lang['scope-management'] = 'Scope Management';
+$lang['schedule-management'] = 'Schedule Management';
+$lang['activity-attributes'] = 'Activity Attributes';
+$lang['activity-list'] = 'Activity List';
+$lang['milestone-list'] = 'Milestone List';
+$lang['cost-management'] = 'Cost Management';
+$lang['activity-cost'] = 'Activity Cost Estimates';
+$lang['quality-management'] = 'Quality Management';
+$lang['process-quality'] = 'Process Quality Checklists';
+$lang['hr-management'] = 'Human Resource Management';
+$lang['communication-management'] = 'Communication Management';
+$lang['risk-management'] = 'Risk Management';
+$lang['change-request'] = 'Change Request';
+$lang['make-decisions'] = 'Make-or-Buy Decisions';
+$lang['stakeholder-management'] = 'Stakeholder Management';
+$lang['requirement-documentati'] = 'Requirement Documentati';
+$lang['requirement-traceability'] = 'Requirement Traceability Matrix';
+$lang['project-schedule-network'] = 'Project Schedule Network';
+$lang['activity-resource'] = 'Activity Resource Requirement';
+$lang['resource-breakdown'] = 'Resource Breakdown Structure';
+$lang['activity-duration'] = 'Activity Duration Estimates';
+$lang['cost-baseline'] = 'Cost Baseline';
+$lang['project-funding'] = 'Project Funding Requirement';
+$lang['product-quality'] = 'Product Quality Checklists';
+$lang['quality-metrics'] = 'Quality Metrics';
+$lang['risk-register'] = 'Risk Register';
+$lang['procurement-management'] = 'Procurement Management';
+$lang['procurement-statement'] = 'Procurement Statement of Work';
+$lang['project-scope'] = 'Project Scope Statement';
+$lang['scope-baseline'] = 'Scope Baseline';
+$lang['project-calendar'] = 'Project Calendar';
+$lang['project-schedule'] = 'Project Schedule';
+$lang['schedule-baseline'] = 'Schedule Baseline';
+$lang['process-improvement'] = 'Process Improvement';
+$lang['project-status'] = 'Project Status Report';
+$lang['deliverables-status'] = 'Deliverables Status';
+$lang['project-team'] = 'Project Team Directory';
+$lang['team-performance'] = 'Team Performance Assessment';
+$lang['enterprise-environment'] = 'Enterprise Environment Factors';
+$lang['procurement-agreement'] = 'Procurement Agreement';
+$lang['issue-log'] = 'Issue Log';
+$lang['change-log'] = 'Change Log';
+$lang['earned-value'] = 'Earned Value Status Report';
+$lang['lessons-learned'] = 'Lessons Learned';
+
+// btn NOTIFICATION BOARD
+$lang['notiFication-board'] = 'NOTIFICATION BOARD';
+
+?>
