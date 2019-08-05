74c5a4d84dfe701af09e866a5a4b2bc8b541873a
Rodrigoblizz <rodrigo.blizzard@hotmail.com>
2019-08-05 17:04:34 -0300

    style sidebar

--- application/views/frame/sidebar_nav_view.php
+++ application/views/frame/sidebar_nav_view.php
@@ -11,7 +11,7 @@
             <a href="<?=base_url()?>"><i class="fa fa-home fa-fw"></i> Dashboard</a>
          </li>
          <?php if($this->session->userdata('role') == 'admin'): ?>
-         <li>
+         <li>z
             <a href="#"><i class="fa fa-user fa-fw"></i> Administrator<span class="fa arrow"></span></a>
             <ul class="nav nav-second-level">
                <li> <a href="<?=base_url('admin/user_list')?>">&raquo; User List</a> </li>
@@ -22,15 +22,15 @@
          <li>
             <a href="#"><i class="fa fa-folder fa-fw"></i> Projects<span class="fa arrow"></span></a>
             <ul class="nav nav-second-level collapse in">
-               <li> <a href="<?=base_url('new')?>">&raquo; New Project</a> </li>
-               <li> <a href="<?=base_url('projects')?>">&raquo; My Projects</a> </li>
+               <li> <a href="<?=base_url('new')?>"> <i class= "fa fa-plus-circle"></i> New Project</a> </li>
+               <li> <a href="<?=base_url('projects')?>"><i class= "fa fa-folder-open"></i> My Projects</a> </li>
             </ul>
          </li>
          <li>
             <a href="#"><i class="fa fa-user fa-fw"></i> Account<span class="fa arrow"></span></a>
             <ul class="nav nav-second-level">
                <li> <a data-toggle="modal" data-target="#myAccount" href="#myAccount">&raquo; My Profile</a> </li>
-            </ul>            
+            </ul>
          </li>
          <!-- fim do main panel -->
       </ul>
