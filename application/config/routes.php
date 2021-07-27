8530dfe557d754038a3a4e5186e7258e40e85d1c
ThiagoCMAraujo <tmelo387@gmail.com>
2021-07-27 11:18:54 -0300

    Criação da rota, controller para a geração do overleaf e model

--- application/config/routes.php
+++ application/config/routes.php
@@ -380,5 +380,4 @@ $route['notification-board/insert'] = "notificationboard/insert";
 $route['notification-board/update'] = "notificationboard/update";
 $route['notification-board/delete/(:num)'] = "notificationboard/delete/$1";
 
-
-
+$route['overleaf-document/new/(:num)'] = "overleaf/document/new/$1";
---
+++ application/controllers/ProjectToOverleafDoc.php
@@ -0,0 +1,70 @@
+<?php
+    class ProjectPerformanceReport_PDF extends CI_Controller{
+
+			function __construct(){
+				parent::__construct();
+
+				if (!$this->session->userdata('logged_in')) {
+					redirect(base_url());
+				}
+
+				//$this->load->helper('url');
+				$this->lang->load('btn', 'english');
+				//$this->lang->load('btn', 'portuguese-brazilian');
+        $this->lang->load('project_performance_report','english');
+				$this->load->model('Project_performance_report_model');
+				$this->load->model('Project_model');
+			}
+
+		function index() {
+	    $this->load->library('Pdf');
+	    $pdf = new Pdf('P', 'mm', 'A4', true, 'UTF-8', false);
+	    $pdf->SetTitle('Change Request');
+	    $pdf->SetHeaderMargin(30);
+	    $pdf->SetTopMargin(20);
+	    $pdf->setFooterMargin(20);
+	    $pdf->SetAutoPageBreak(true);
+	    $pdf->SetAuthor('Author');
+	    $pdf->SetDisplayMode('real', 'default');
+	    $pdf->Write(5, 'CodeIgniter TCPDF Integration');
+	    //$pdf->Output('tap.pdf', 'I'); }
+			$this->load->view('pdf/project_performance_report_pdf');
+    }
+
+		function exportLatex($project_id) {
+			//Configuring File Latex
+			$file = "";
+
+			$file .= "\documentclass[twoside,a4paper,11pt]{book}\n";
+		$file .= "\usepackage[utf8]{inputenc}\n";
+		$file .= "\usepackage{graphicx}\n";
+		$file .= "\usepackage{booktabs}\n";
+		$file .= "\usepackage{float}\n";
+		$file .= "\usepackage[alf]{abntex2cite}\n";
+		$file .= "\usepackage[brazilian,hyperpageref]{backref}\n";
+
+
+		$file .= "\renewcommand{\backrefpagesname}{Citado na(s) página(s):~}";
+		$file .= "\renewcommand{\backref}{}";
+		$file .= "\\renewcommand*{\backrefalt}[4]{\n";
+		$file .= "\ifcase #1 %\n";
+		$file .= "Nenhuma citação no texto.%\n";
+		$file .= "\or\n";
+		$file .= "Citado na página #2.%\n";
+		$file .= "\\else\n";
+		$file .= "Citado #1 vezes nas páginas #2.%\n";
+		$file .= "\\fi}%\n";
+		$file .= "\n";
+
+		$file .= "\\title {." . $project->getTitleById() ."}\n";
+
+		
+
+
+
+
+
+
+		}
+}
+    ?>
