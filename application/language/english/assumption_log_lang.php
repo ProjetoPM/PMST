4698770be0306c00d961309dd4d8c613a4cb70df
ThiagoCMAraujo <thiagoaraujo.aluno@unipampa.edu.br>
2021-09-03 14:10:21 -0300

    Criação da lang em portugues e adição de variáveis para substituir textos escritos manualmente - Assumption Log

--- application/language/english/assumption_log_lang.php
+++ application/language/english/assumption_log_lang.php
@@ -6,7 +6,8 @@ $lang['acl_title'] = 'Assumption Log';
 //Form
 $lang['acl_description_log'] = 'Description Log';
 $lang['acl_type_C'] = 'Constraint Log';
-
+$lang['acl_assumption'] = 'Assumption';
+$lang['acl_constraint'] = 'Constraint';
 
 //Tooltip
 $lang['acl_description_log_tooltip'] = '';
---
+++ application/language/portuguese-brazilian/assumption_log_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+// Title
+$lang['acl_title'] = 'Registro de premissas';
+
+//Form
+$lang['acl_description_log'] = 'Descrição de Registro';
+$lang['acl_type_C'] = 'Registro de Restrições';
+$lang['acl_assumption'] = 'Premissa';
+$lang['acl_constraint'] = 'Restrição';
+
+//Tooltip
+$lang['acl_description_log_tooltip'] = '';
+
+
+
+?>
