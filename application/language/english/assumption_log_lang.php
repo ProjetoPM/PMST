331490a2175f46587ac8fbe71309fb8294dec621
ThiagoCMAraujo <thiagoaraujo.aluno@unipampa.edu.br>
2021-09-07 23:24:37 -0300

    Adição de mais variáveis - Assumption Log

--- application/language/english/assumption_log_lang.php
+++ application/language/english/assumption_log_lang.php
@@ -9,6 +9,12 @@ $lang['acl_type_C'] = 'Constraint Log';
 $lang['acl_assumption'] = 'Assumption';
 $lang['acl_constraint'] = 'Constraint';
 
+// List
+$lang['acl_type'] = 'Type';
+$lang['acl_description'] = 'Description';
+$lang['acl_new_assumption']= ' New Assumption';
+$lang['acl_new_constraint'] = ' New Constraint';
+
 //Tooltip
 $lang['acl_description_log_tooltip'] = '';
 
--- application/language/portuguese-brazilian/assumption_log_lang.php
+++ application/language/portuguese-brazilian/assumption_log_lang.php
@@ -9,6 +9,12 @@ $lang['acl_type_C'] = 'Registro de Restrições';
 $lang['acl_assumption'] = 'Premissa';
 $lang['acl_constraint'] = 'Restrição';
 
+// List
+$lang['acl_type'] = 'Tipo';
+$lang['acl_description'] = 'Descrição';
+$lang['acl_new_assumption']= ' Nova Premissa';
+$lang['acl_new_constraint'] = ' Nova Restrição';
+
 //Tooltip
 $lang['acl_description_log_tooltip'] = '';
 
