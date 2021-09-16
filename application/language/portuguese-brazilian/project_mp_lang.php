6dcd0ba93df8dfb8b8de931e0da99e260b08d1b7
ThiagoCMAraujo <thiagoaraujo.aluno@unipampa.edu.br>
2021-09-16 10:06:59 -0300

    Criação das langs pt

---
+++ application/language/portuguese-brazilian/project_mp_lang.php
@@ -0,0 +1,65 @@
+<?php
+
+// Title
+$lang['pmp_title'] = 'Plano de Gerenciamento do Projeto';
+
+//Form
+$lang['subsidiary_mp'] = 'Planos de Gerenciamento Auxiliares';
+$lang['pmp_guidelines'] = 'Plano de gerenciamento do escopo';
+$lang['pmp_requirements_mp'] = 'Plano de gerenciamento dos requisitos';
+$lang['pmp_schedule_mp'] = 'Plano de gerenciamento do cronograma';
+$lang['pmp_cost_mp'] = 'Plano de gerenciamento dos custos';
+$lang['pmp_quality_mp'] = 'Plano de gerenciamento da qualidade';
+$lang['pmp_resource_mp'] = 'Plano de gerenciamento dos recursos';
+$lang['pmp_stakeholders'] = 'Plano de gerenciamento das comunicações';
+$lang['pmp_risk_mp'] = 'Plano de gerenciamento dos riscos';
+$lang['pmp_procurement_mp'] = 'Plano de gerenciamento das aquisições';
+$lang['pmp_stakeholder_mp'] = 'Plano de engajamento das partes interessadas';
+
+$lang['baseline'] = 'Linhas de Base';
+$lang['pmp_scope_baseline'] = 'Linha de Base do Escopo';
+$lang['pmp_baseline'] = 'Linha de Base do Cronograma';
+$lang['pmp_cost_baseline'] = 'Linha de Base dos Custos';
+
+$lang['additional_components'] = 'Componentes Adicionais';
+$lang['pmp_change_mp'] = 'Plano de Gerenciamento de Mudanças';
+$lang['pmp_configuration_mp'] = 'Plano de Gerenciamento de Configuração';
+$lang['pmp_performance'] = 'Medição do Desempenho De Linha de Base';
+$lang['pmp_lifecycle'] = 'Ciclo de Vida do Projeto*';
+$lang['pmp_development'] = 'Abordagem de Desenvolvimento';
+$lang['pmp_key_review'] = 'Revisões de gerenciamento';
+
+
+//Tooltip
+$lang['pmp_guidelines_tp'] = 'Estabelece como o escopo será definido,
+desenvolvido, monitorado, controlado e validado';
+$lang['pmp_requirements_mp_tp'] = 'Estabelece como os requisitos serão
+analisados, documentados e gerenciados';
+$lang['pmp_schedule_mp_tp'] = ' Estabelece os critérios e as atividades
+para desenvolvimento, monitoramento e controle do cronograma';
+$lang['pmp_cost_mp_tp'] = 'Estabelece como os custos serão planejados,
+estruturados e controlados';
+$lang['pmp_quality_mp_tp'] = 'Estabelece como as políticas da qualidade,
+metodologias e padrões de uma organização serão implementados no projeto';
+$lang['pmp_resource_mp_tp'] = 'Fornece orientação sobre como os recursos
+do projeto devem ser categorizados, alocados, gerenciados e liberados';
+$lang['pmp_stakeholders_tp'] = ' Estabelece como, quando e por
+quem informações sobre o projeto serão administradas e divulgadas';
+$lang['pmp_risk_mp_tp'] = ' Estabelece como as atividades de
+gerenciamento de risco serão estruturadas e realizadas';
+$lang['pmp_procurement_mp_tp'] = ' Estabelece como a equipe do projeto irá
+adquirir bens e serviços de fora da organização executora';
+
+$lang['pmp_scope_baseline_tp'] = 'A versão aprovada de uma declaração de escopo,
+estrutura analítica do projeto (EAP) e seu dicionário EAP associado, que é usada como base para comparação';
+$lang['pmp_baseline_tp'] = '. The approved version of the schedule model that is used as
+a basis for comparison to the actual results';
+$lang['pmp_cost_baseline_tp'] = 'The approved version of the time-phased project budget that is
+used as a basis for comparison to the actual results';
+
+$lang['pmp_change_mp_tp'] = 'Describes how the change requests throughout the project will be formally authorized and incorporated.';
+$lang['pmp_configuration_mp_tp'] = 'Describes how the information about the items of the project (and which items) will be recorded and updated so that the product, service, or result of the project remains consistent and/or operative.';
+$lang['pmp_performance_tp'] = 'An integrated scope-schedule-cost plan for the project work against which project execution is compared to measure and manage performance.';
+$lang['pmp_lifecycle_tp'] = 'Describes the series of phases that a project passes through from its initiation to its closure.';
+$lang['pmp_development_tp'] = 'Describes the product, service, or result development approach, such as predictive, iterative, agile, or a hybrid model.';
+$lang['pmp_key_review_tp'] = 'Identifies the points in the project when the project manager and relevant stakeholders will review the project progress to determine if performance is as expected, or if preventive or corrective actions are necessary.';
