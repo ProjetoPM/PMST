6ed5d96c7ec8e25e9e7c59b0989a84560a6ee3f3
tmelo387 <tmelo387@gmail.com>
2021-11-08 17:59:15 -0300

    globalização - scope specification

--- application/language/portuguese-brazilian/scope_specification_lang.php
+++ application/language/portuguese-brazilian/scope_specification_lang.php
@@ -1,22 +1,27 @@
 <?php
 
 // Title
-$lang['scope_spec-title'] = 'Escopo';
+$lang['pss-title'] = 'Declaração do Escopo do Projeto';
 
 //Form 
-$lang['scope_spec-desc'] = 'portugues';
-$lang['scope_spec-accept'] = 'portugues';
-$lang['scope_spec-deli'] = 'portugues';
-$lang['scope_spec-exclu'] = 'portugues';
-$lang['scope_spec-rest'] = 'portugues';
-$lang['scope_spec-assumpt'] = 'portugues';
+$lang['pss-desc'] = 'Descrição do Escopo do Produto';
+$lang['pss-accept'] = 'Critérios de Aceitação';
+$lang['pss-deli'] = 'Entregas';
+$lang['pss-exclu'] = 'Exclusões do Projeto';
+
 
 
 //Tooltip
-$lang['scope_spec-desc-tooltip'] = 'portugues';
-$lang['scope_spec-accept-tooltip'] = 'portugues';
-$lang['scope_spec-deli-tooltip'] = 'portugues';
-$lang['scope_spec-exclu-tooltip'] = 'portugues';
-$lang['scope_spec-rest-tooltip'] = 'portugues';
-$lang['scope_spec-assumpt-tooltip'] = 'portugues';
+$lang['pss-desc-tp'] = 'Elabora progressivamente as características do produto, serviço ou resultado
+descritas no termo de abertura do projeto e na documentação dos requisitos.';
+$lang['pss-accept-tp'] = ' Um conjunto de condições que precisam ser atendidas antes que as entregas
+sejam aceitas.';
+$lang['pss-deli-tp'] = 'Qualquer produto, resultado ou capacidade de realizar um serviço, que seja único, verificável e
+necessário para concluir um processo, fase ou projeto. As entregas também incluem os resultados auxiliares,
+tais como relatórios e documentação de gerenciamento do projeto. Essas entregas podem ser descritas em nível
+conciso ou em grande detalhe.';
+$lang['pss_exclu_tp'] = 'Identifica o que é excluído do projeto. Declarar explicitamente o que está fora do escopo do
+projeto ajuda no gerenciamento das expectativas das partes interessadas e pode reduzir a distorção de escopo.';
+$lang['pss-rest-tp'] = 'Report format Example';
+$lang['pss-assumpt-tp'] = 'Report format Example';
 ?>
