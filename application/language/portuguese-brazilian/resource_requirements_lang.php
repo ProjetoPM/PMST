a40169ba5943023f88cf3cc35ee98a532b049960
tmelo387 <tmelo387@gmail.com>
2021-11-10 11:22:41 -0300

    globalização - resource requirements

---
+++ application/language/portuguese-brazilian/resource_requirements_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+// Title
+$lang['rr_title'] = 'Requisitos de Recursos';
+//Form
+$lang['activity_name'] = 'Nome da Atividade';
+$lang['rr_resource_description'] = 'Descrição do Recurso';
+$lang['rr_required_amount_of_resource'] = 'Quantidade de Recurso Requerida';
+$lang['rr_resource_cost_per_unit'] = 'Custo por Unidade do Recurso';
+$lang['rr_resource_type'] = 'Tipo do Recurso';
+
+
+//Tooltip
+$lang['rr_resource_description_tp'] = '';
+$lang['rr_required_amount_of_resource_tp'] = '';
+$lang['rr_resource_cost_per_unit_tp'] = '';
+$lang['rr_resource_type_tp'] = '';
+
+
+
+?>
