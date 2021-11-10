ed8d0d995f4325745b2dadd3ffb2e069308e98a6
tmelo387 <tmelo387@gmail.com>
2021-11-09 22:35:34 -0300

    globalização - scope management plan

--- application/views/project/scope/scope_mp/edit.php
+++ application/views/project/scope/scope_mp/edit.php
@@ -26,7 +26,7 @@
                         <div class="panel-body">
                             <h1 class="page-header">
 
-                                <?= $this->lang->line('scomp_title')  ?>
+                                <?= $this->lang->line('smp-title')  ?>
 
                             </h1>
                             <?php
@@ -38,45 +38,45 @@
                                     <input type="hidden" name="status" value="1">
 
                                     <div class=" col-lg-12 form-group">
-                                        <label for="scope_specification"><?= $this->lang->line('smp_scope_specification') ?></label>
+                                        <label for="scope_specification"><?= $this->lang->line('smp-scope_specification') ?></label>
                                         <span class="smp_1">2000</span><?= $this->lang->line('character') ?>
-                                        <a class="btn-sm btn-default" id="smp_tp_1" data-toggle="tooltip" data-placement="right" title="<?= $this->lang->line('smp_scope_specification_tp') ?>"><i class="glyphicon glyphicon-comment"></i></a>
+                                        <a class="btn-sm btn-default" id="smp_tp_1" data-toggle="tooltip" data-placement="right" title="<?= $this->lang->line('smp-scope_specification-tp') ?>"><i class="glyphicon glyphicon-comment"></i></a>
                                         <div>
                                         <textarea onkeyup="limite_textarea(this.value, 'smp_1')" id="smp_txt_1" maxlength="2000" oninput="eylem(this, this.value)" class="form-control elasticteste" name="scope_specification"><?= $smp->scope_specification; ?></textarea>
                                         </div>
                                     </div>
 
                                     <div class=" col-lg-12 form-group">
-                                        <label for="eap_process"><?= $this->lang->line('smp_eap_process') ?></label>
+                                        <label for="eap_process"><?= $this->lang->line('smp-eap_process') ?></label>
                                         <span class="smp_2">2000</span><?= $this->lang->line('character') ?>
-                                        <a class="btn-sm btn-default" id="smp_tp_2" data-toggle="tooltip" data-placement="right" title="<?= $this->lang->line('smp_eap_process_tp') ?>"><i class="glyphicon glyphicon-comment"></i></a>
+                                        <a class="btn-sm btn-default" id="smp_tp_2" data-toggle="tooltip" data-placement="right" title="<?= $this->lang->line('smp-eap_process-tp') ?>"><i class="glyphicon glyphicon-comment"></i></a>
                                         <div>
                                         <textarea onkeyup="limite_textarea(this.value, 'smp_2')" id="smp_txt_2" maxlength="2000" oninput="eylem(this, this.value)" class="form-control elasticteste" name="eap_process"><?= $smp->eap_process; ?></textarea>
                                         </div>
                                     </div>
 
                                     <div class=" col-lg-12 form-group">
-                                        <label for="deliveries_acceptance"><?= $this->lang->line('smp_deliveries_acceptance') ?></label>
+                                        <label for="deliveries_acceptance"><?= $this->lang->line('smp-deliveries_acceptance') ?></label>
                                         <span class="smp_3">2000</span><?= $this->lang->line('character') ?>
-                                        <a class="btn-sm btn-default" id="smp_tp_3" data-toggle="tooltip" data-placement="right" title="<?= $this->lang->line('smp_deliveries_acceptance_tp') ?>"><i class="glyphicon glyphicon-comment"></i></a>
+                                        <a class="btn-sm btn-default" id="smp_tp_3" data-toggle="tooltip" data-placement="right" title="<?= $this->lang->line('smp-deliveries_acceptance-tp') ?>"><i class="glyphicon glyphicon-comment"></i></a>
                                         <div>
                                         <textarea onkeyup="limite_textarea(this.value, 'smp_3')" id="smp_txt_3" maxlength="2000" oninput="eylem(this, this.value)" class="form-control elasticteste" name="deliveries_acceptance"><?= $smp->deliveries_acceptance; ?></textarea>
                                         </div>
                                     </div>
 
                                     <div class=" col-lg-12 form-group">
-                                        <label for="scope_change_mp"><?= $this->lang->line('smp_scope_change_mp') ?></label>
+                                        <label for="scope_change_mp"><?= $this->lang->line('smp-scope_change_mp') ?></label>
                                         <span class="smp_4">2000</span><?= $this->lang->line('character') ?>
-                                        <a class="btn-sm btn-default" id="smp_tp_4" data-toggle="tooltip" data-placement="right" title="<?= $this->lang->line('smp_scope_change_mp_tp') ?>"><i class="glyphicon glyphicon-comment"></i></a>
+                                        <a class="btn-sm btn-default" id="smp_tp_4" data-toggle="tooltip" data-placement="right" title="<?= $this->lang->line('smp-scope_change_mp-tp') ?>"><i class="glyphicon glyphicon-comment"></i></a>
                                         <div>
                                         <textarea onkeyup="limite_textarea(this.value, 'smp_4')" id="smp_txt_4" maxlength="2000" oninput="eylem(this, this.value)" class="form-control elasticteste" name="scope_change_mp"><?= $smp->scope_change_mp; ?></textarea>
                                         </div>
                                     </div>
 
                                     <div class="col-lg-12 form-group">
-                                        <label for="baseline"><?= $this->lang->line('smp_baseline') ?></label>
+                                        <label for="baseline"><?= $this->lang->line('smp-baseline') ?></label>
                                         <span class="smp_5">2000</span><?= $this->lang->line('character') ?>
-                                        <a class="btn-sm btn-default" id="smp_tp_5" data-toggle="tooltip" data-placement="right" title="<?= $this->lang->line('smp_baseline_tp') ?>"><i class="glyphicon glyphicon-comment"></i></a>
+                                        <a class="btn-sm btn-default" id="smp_tp_5" data-toggle="tooltip" data-placement="right" title="<?= $this->lang->line('smp-baseline-tp') ?>"><i class="glyphicon glyphicon-comment"></i></a>
                                         <div>
                                         <textarea onkeyup="limite_textarea(this.value, 'smp_5')" id="smp_txt_5" maxlength="2000" oninput="eylem(this, this.value)" class="form-control elasticteste" name="baseline"><?= $smp->baseline; ?></textarea>
                                         </div>
