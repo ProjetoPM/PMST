08685dc21a94de15c72e3a0188c0c083e5f0d7c2
tmelo387 <tmelo387@gmail.com>
2021-11-10 13:03:12 -0300

    resource mp - globalização

--- application/language/portuguese-brazilian/resource_lang.php
+++ application/language/portuguese-brazilian/resource_lang.php
@@ -1,16 +1,48 @@
 <?php
 
 // Title
-$lang['human_resource-title'] = 'Recursos Humanos';
+$lang['remp_title'] = 'Resource Management Plan';
 
 //Form 
-$lang['human_resource-roles'] = 'Funções e Responsabilidades';
-$lang['human_resource-chart'] = 'Organograma';
-$lang['human_resource-staff'] = 'Funcionários';
+$lang['remp_roles'] = 'Papéis e Responsabilidades';
+$lang['remp_chart'] = 'Organogramas do Projeto';
+$lang['remp_staff'] = 'Desenvolvimento de Equipes';
+$lang['remp_identification_resources'] = 'Identificação dos Recursos';
+$lang['remp_acquiring_resources'] = 'Adquirir Recursos';
+$lang['remp_team'] ='Gerenciamento dos Recursos da Equipe do Projeto';
+$lang['remp_training'] = 'Treinamento';
+$lang['remp_control'] = 'Controle de Recursos';
+$lang['remp_plan'] = 'Plano de Reconhecimento';
 
 //Tooltip
-$lang['human_resource-roles-tooltip'] = 'Funções e Responsabilidades Exemplo';
-$lang['human_resource-chart-tooltip'] = 'Organograma Exemplo';
-$lang['human_resource-staff-tooltip'] = 'Funcionários Exemplo';
+$lang['remp_roles_tp'] = 'Papel: A função assumida ou a ser designada a uma pessoa no projeto. Exemplos de papéis de projeto são
+engenheiro civil, analista de negócios e coordenador de testes. Autoridade: O direito de aplicar recursos do projeto, tomar decisões, assinar aprovações, aceitar entregas
+e influenciar outras pessoas para executar o trabalho do projeto. Exemplos de decisões que precisam de
+autoridade clara incluem a seleção de um método para concluir uma atividade, critérios para aceitação da
+qualidade e como responder às variações no projeto. Os membros da equipe atuam melhor quando seus
+níveis de autoridade individuais correspondem às suas responsabilidades individuais.';
 
+$lang['remp_chart_tp'] = 'Um organograma do projeto é uma exibição gráfica dos membros da equipe
+do projeto e suas relações hierárquicas. Pode ser formal ou informal, altamente detalhado ou amplamente
+estruturado, dependendo das necessidades do projeto. Por exemplo, o organograma do projeto para uma equipe
+de resposta a desastres com 3.000 pessoas terá mais detalhes do que um organograma de um projeto interno
+com 20 pessoas';
+
+$lang['remp_staff_tp'] = 'Métodos para desenvolver a equipe do projeto.';
+
+$lang['remp_team_tp'] = 'Orientação sobre como os recursos da equipe do projeto
+devem ser definidos, mobilizados, gerenciados e, por fim, liberados.';
+
+$lang['remp_training_tp'] ='. Estratégias de treinamento para membros de equipes.';
+
+$lang['remp_acquiring_resources_tp'] ='. Orientação sobre como adquirir recursos físicos e de equipe para o projeto.';
+
+$lang['remp_identification_resources_tp'] = ' Métodos para identificar e quantificar os recursos físicos e de equipe necessários.';
+
+$lang['remp_control_tp'] = 'Métodos para garantir que recursos físicos adequados estejam disponíveis conforme
+necessário e que a aquisição de recursos físicos seja otimizada para as necessidades do projeto. Inclui
+informações sobre gerenciamento de estoques, equipamentos e suprimentos durante o ciclo de vida do projeto.';
+
+$lang['remp_plan_tp'] = 'Quais reconhecimentos e recompensas serão concedidos aos membros da equipe,
+e quando estes serão concedidos.';
 ?>
