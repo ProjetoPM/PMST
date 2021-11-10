044307fbd8f3db504ee9558c26661528555bc72b
tmelo387 <tmelo387@gmail.com>
2021-11-10 10:52:37 -0300

    globalização - schedule network diagram

--- application/controllers/ProjectScheduleNetworkDiagram.php
+++ application/controllers/ProjectScheduleNetworkDiagram.php
@@ -10,25 +10,38 @@ class ProjectScheduleNetworkDiagram extends CI_Controller
 		if (!$this->session->userdata('logged_in')) {
 			redirect(base_url());
 		}
+		if (strcmp($_SESSION['language'], "US") == 0) {
+            $this->lang->load('schedule_net', 'english');
+            $this->lang->load('project-page', 'english');
+        } else {
+            $this->lang->load('schedule_net', 'portuguese-brazilian');
+            $this->lang->load('project-page', 'portuguese-brazilian');
+        }
+
 		$this->load->helper('url');
 		$this->load->model('Activity_model');
 		$this->load->model('view_model');
 		$this->load->model('log_model');
 		$this->load->helper('log_activity');
-
-
-		$this->lang->load('btn', 'english');
+		
+		
 		// $this->lang->load('btn','portuguese-brazilian');
-		$this->lang->load('schedule_net', 'english');
-
+		
+		
 		// $this->lang->load('manage-cost','portuguese-brazilian');
-
+		
 	}
-
-
+	
+	
 	//SCHEDULE NETWORK
 	public function list($project_id)
 	{
+		if (strcmp($_SESSION['language'], "US") == 0) {
+			$this->lang->load('btn', 'english');
+		} else {
+			$this->lang->load('btn', 'portuguese-brazilian');
+		}
+
 		$dado['project_id'] = $project_id;
 		$dado['activity'] = $this->Activity_model->getAll($project_id);
 		$this->load->view('frame/header_view');
@@ -36,29 +49,35 @@ class ProjectScheduleNetworkDiagram extends CI_Controller
 		$this->load->view('frame/sidebar_nav_view');
 		$this->load->view('project/schedule/schedule_network/list', $dado);
 	}
-
+	
 	public function edit($project_id)
 	{
+		if (strcmp($_SESSION['language'], "US") == 0) {
+			$this->lang->load('btn', 'english');
+		} else {
+			$this->lang->load('btn', 'portuguese-brazilian');
+		}
+		
 		$query['activity'] = $this->Activity_model->get($project_id);
-
+		
 		$this->load->view('frame/header_view.php');
 		$this->load->view('frame/topbar');
 		$this->load->view('frame/sidebar_nav_view.php');
 		$this->load->view('project/schedule/schedule_network/edit', $query);
 	}
-
+	
 	public function update($project_id)
 	{
 		$activity['predecessor_activity'] = $this->input->post('predecessor_activity');
 		$activity['dependence_type'] = $this->input->post('dependence_type');
 		$activity['anticipation'] = $this->input->post('anticipation');
 		$activity['wait'] = $this->input->post('wait');
-
+		
 		$activity['project_id'] = $this->input->post('project_id');
-
+		
 		$data['activity'] = $activity;
 		$query = $this->Activity_model->update($data['activity'], $project_id);
-
+		
 		if ($query) {
 			insertLogActivity('update', 'project schedule network diagram');
 			$this->session->set_flashdata('success', 'Project Schedule Network Diagram has been successfully changed!');
@@ -66,3 +85,4 @@ class ProjectScheduleNetworkDiagram extends CI_Controller
 		}
 	}
 }
+
---
+++ application/language/portuguese-brazilian/schedule_net_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+// Title
+$lang['snd_title'] = 'Schedule Network Diagram';
+
+//Form
+$lang['activity_name'] = 'Nome da Atividade';
+$lang['snd_predecessor_activity'] = 'Atividade Predecessora';
+$lang['snd_dependence_type'] = 'Tipo de Dependência';
+$lang['snd_anticipation'] = 'Antecipação';
+$lang['snd_wait'] = 'Espera';
+
+
+
+//Tooltip
+$lang['snd_predecessor_activity_tp'] = '';
+$lang['snd_dependence_type_tp'] = '';
+$lang['snd_anticipation_tp'] = '';
+$lang['snd_wait_tp'] = '';
+
+
+?>
