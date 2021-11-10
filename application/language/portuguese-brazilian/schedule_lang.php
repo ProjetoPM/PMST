4ae8a18a54754e6a7d1eb95194115b5fbf949c00
tmelo387 <tmelo387@gmail.com>
2021-11-10 10:45:46 -0300

    globalização - schedule mp

--- application/language/portuguese-brazilian/schedule_lang.php
+++ application/language/portuguese-brazilian/schedule_lang.php
@@ -1,6 +1,55 @@
 <?php
 
 // Title
-$lang['schedule-title'] = 'Cronograma';
+$lang['schmp-title'] = 'Plano de Gerenciamento do Cronograma';
+
+
+//Form
+$lang['schmp-model'] = 'Desenvolvimento do Modelo do Cronograma de Projeto';
+$lang['schmp-length'] = 'Duração do Lançamento e Interação';
+$lang['schmp-accuracy_level'] = 'Nível de Exatidão';
+$lang['schmp-measure'] = 'Unidades de Medida';
+$lang['schmp-procedures'] = 'Associações com Procedimentos Organizacionais';
+$lang['schmp-maintenance'] = 'Manutenção do Modelo do Cronograma do Projeto';
+$lang['schmp-control'] = 'Limites de Controle';
+$lang['schmp-rules'] = 'Regras para Medição do Desempenho';
+$lang['schmp-format'] = 'Formatos de Relatórios';
+
+//Tooltip
+$lang['schmp-model-tp'] = 'A metodologia e a ferramenta de cronograma a
+serem usadas no desenvolvimento do modelo do cronograma do projeto';
+
+$lang['schmp-length-tp'] = 'Quando se usa um ciclo de vida adaptativo, as durações fixas das iterações
+para lançamentos e ondas são especificadas. As durações fixas das iterações são a duração na qual a equipe
+trabalha continuamente no sentido da conclusão de uma meta. Fixar a duração das iterações ajuda a minimizar
+a distorção de escopo, uma vez que obriga as equipes a trabalharem nas funcionalidades essenciais primeiro e
+depois em outras funcionalidades quando o tempo permitir';
+
+$lang['schmp-accuracy_level-tp'] = 'O nível de exatidão especifica a faixa aceitável usada na determinação de estimativas realistas
+de duração das atividades e pode incluir uma quantidade para contingências';
+
+$lang['schmp-measure-tp'] = 'Cada unidade de medida (como horas e dias de pessoal ou semanas para medidas de
+tempo, ou metros, litros, toneladas, quilômetros ou jardas cúbicas para medidas de quantidade) é definida para
+cada um dos recursos';
+
+$lang['schmp-procedures-tp'] = 'A estrutura analítica do projeto (EAP) (Seção 5.4) fornece
+a estrutura para o plano de gerenciamento do cronograma, permitindo a consistência com as estimativas e os
+cronogramas resultantes';
+
+$lang['schmp-maintenance-tp'] = 'O processo usado para atualizar o status e registrar o
+andamento do projeto no modelo de cronograma durante a execução do projeto é definido';
+
+$lang['schmp-control-tp'] = ' Limites de variação para o monitoramento do desempenho do cronograma podem ser
+especificados para indicar uma quantidade acordada de variação a ser permitida antes que alguma ação precise
+ser tomada. Os limites são tipicamente expressos como desvios percentuais dos parâmetros estabelecidos no
+plano da linha de base';
+
+$lang['schmp-rules-tp'] = 'As regras para medição do desempenho do gerenciamento do valor
+agregado (GVA) ou outras regras de medição física do desempenho são estabelecidas. Por exemplo, o plano de
+gerenciamento do cronograma pode especificar: Regras para estabelecer o percentual completo, Técnicas de GVA (por exemplo, linhas de base, fórmula fixa, percentual completo, etc.) a serem empregadas
+(para obter informações mais específicas, consulte Practice Standard for Earned Value Management [17]), e Medições do desempenho do cronograma, como a variação de prazos (VPR) e o índice de desempenho de
+prazos (IDP), usados para avaliar a magnitude de variação à linha de base do cronograma original';
+
+$lang['schmp-report_format-tp'] = 'Os formatos e a frequência dos vários relatórios de cronograma são definidos';
 
 ?>
