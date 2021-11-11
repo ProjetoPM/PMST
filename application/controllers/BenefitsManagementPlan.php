5119ad4f25142f350acb8b4bc7b31243212bb581
tmelo387 <tmelo387@gmail.com>
2021-11-11 16:55:50 -0300

    globalização feedback - Project Charter

--- application/controllers/BenefitsManagementPlan.php
+++ application/controllers/BenefitsManagementPlan.php
@@ -81,6 +81,12 @@ class BenefitsManagementPlan extends CI_Controller
 
 	function insert()
 	{
+
+		if(strcmp($_SESSION['language'],"US") == 0){
+			$feedback_success = 'Item Created';
+        }else{
+			$feedback_success = 'Item Criado ';
+		}
 		//$this->ajax_checking();
 
 		insertLogActivity('insert', 'benefits management plan');
@@ -88,7 +94,7 @@ class BenefitsManagementPlan extends CI_Controller
 		$postData = $this->input->post();
 		$insert   = $this->Benefits_plan_model->insert($postData);
 		if ($insert) {
-			$this->session->set_flashdata('success', 'Benefits Management Plan has been successfully created!');
+			$this->session->set_flashdata('success', $feedback_success);
 		}
 		redirect('integration/benefits-mp/edit/' . $postData['project_id']);
 		echo json_encode($insert);
@@ -97,6 +103,12 @@ class BenefitsManagementPlan extends CI_Controller
 
 	public function update()
 	{
+		if(strcmp($_SESSION['language'],"US") == 0){
+			$feedback_success = 'Item Updated';
+        }else{
+			$feedback_success = 'Item Atualizado ';
+		}
+
 		$benefits_plan['target_benefits'] = $this->input->post('target_benefits');
 		$benefits_plan['strategic_alignment'] = $this->input->post('strategic_alignment');
 		$benefits_plan['schedule_benefit'] = $this->input->post('schedule_benefit');
@@ -108,7 +120,7 @@ class BenefitsManagementPlan extends CI_Controller
 		//$insert = $this->project_model->insert_project_pgq($quality_mp);
 		$query = $this->Benefits_plan_model->update($benefits_plan,  $_SESSION['project_id']);
 		if ($query) {
-			$this->session->set_flashdata('success', 'Benefits Management Plan has been successfully changed!');
+			$this->session->set_flashdata('success', $feedback_success);
 			insertLogActivity('update', 'benefits management plan');
 		}
 		redirect('integration/benefits-mp/edit/' . $_SESSION['project_id']);
