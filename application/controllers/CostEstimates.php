5a795b4adc5f10a2d4ee7d98df18d3c5ce4b0482
tmelo387 <tmelo387@gmail.com>
2021-11-11 18:02:07 -0300

    cost planning globalização

--- application/controllers/CostEstimates.php
+++ application/controllers/CostEstimates.php
@@ -68,6 +68,13 @@ class CostEstimates extends CI_Controller
 	
 	public function update($project_id)
 	{
+
+		if(strcmp($_SESSION['language'],"US") == 0){
+			$feedback_success = 'Item Updated';
+        }else{
+			$feedback_success = 'Item Atualizado ';
+		}
+
 		$activity['estimated_cost'] = $this->input->post('estimated_cost');
 		$activity['cumulative_estimated_cost'] = $this->input->post('cumulative_estimated_cost');
 		$activity['replanted_cost'] = $this->input->post('replanted_cost');
@@ -88,7 +95,7 @@ class CostEstimates extends CI_Controller
 
 		if ($query) {
 			insertLogActivity('update', 'cost estimates');
-			$this->session->set_flashdata('success', 'Cost Estimates has been successfully changed!');
+			$this->session->set_flashdata('success', $feedback_success);
 			redirect('cost/cost-estimates/list/' . $activity['project_id']);
 		}
 	}
