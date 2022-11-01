82664085ccad5c42e1a41bba40f1697e3ab3be99
= <tmelo387@gmail.com>
2022-11-01 14:06:21 -0300

    Fix(lang): Risk Register

--- application/language/portuguese-brazilian/risk_lang.php
+++ application/language/portuguese-brazilian/risk_lang.php
@@ -11,6 +11,24 @@ $lang['rir_event'] = 'Evento';
 $lang['rir_date'] = 'Data de identificação';
 $lang['rir_identifier'] = 'Identificador';
 $lang['rir_type'] = 'Tipo de Risco';
+$lang['rir_probability'] = 'Probabilidade';
+$lang['rir_impact'] = 'Impacto';
+$lang['rir_score'] = 'Pontuação do Risco';
+$lang['rir_effects'] = 'Efeitos nos Objetivos';
+$lang['rir_strategy'] = 'Estratégia para o Risco';
+$lang['rir_causes'] = 'Causas';
+$lang['rir_triggers'] = 'Riscos Desencadeados';
+$lang['rir_responses_owner'] = 'Potencias Responsáveis pelo Risco';
+$lang['rir_responses'] = 'Lista de Potenciais Repostas para o Risco';
+$lang['rir_owners'] = 'Potencias Responsáveis pelo Risco';
+$lang['rir_timing'] = 'Prontidão de Resposta';
+$lang['rir_residual'] = 'Riscos Residuais';
+$lang['rir_secondary'] = 'Riscos Secundários';
+$lang['rir_contingency'] = 'Planos de Contingência';
+$lang['rir_contingency_owner'] = 'Responsáveis pelos Planos de Contingência';
+$lang['rir_fallback'] = 'Plano Alternativo';
+$lang['rir_category'] = 'Categoria do Risco';
+$lang['rir_lessons'] = 'Lições Aprendidas';
 
 //options
 $lang['rir_priority-low'] = 'Baixa';
