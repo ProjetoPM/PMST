da2d9696cc10aebfae4226232ec478bbc97136b2
= <tmelo387@gmail.com>
2022-12-06 13:46:50 -0300

    Fix(Lang)

--- application/language/portuguese-brazilian/delivery_acceptance_term_lang.php
+++ application/language/portuguese-brazilian/delivery_acceptance_term_lang.php
@@ -3,11 +3,11 @@
 $lang['ds_title'] = 'Status de Entregas';
 
 //Form
-$lang['ds_validator_name'] = 'Validator Name';
-$lang['ds_role'] = 'Role';
-$lang['ds_function'] = 'Function in the Project';
-$lang['ds_validation_date'] = 'Validation Date';
-$lang['ds_comments'] = 'Validator Comments';
+$lang['ds_validator_name'] = 'Responsável pela Validação';
+$lang['ds_role'] = 'Perfil';
+$lang['ds_function'] = 'Função no Projeto';
+$lang['ds_validation_date'] = 'Data de Validação';
+$lang['ds_comments'] = 'Comentários do Responsável';
 
 
 //Tooltip
